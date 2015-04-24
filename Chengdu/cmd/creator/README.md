f49a09a3834c652f7b9b4a25aeba19a7329d8a99
liugang <lewgun@gmail.com>
2015-04-24 18:12:38 +0800

    add some texture method

--- Chengdu/cmd/creator/README.md
+++ Chengdu/cmd/creator/README.md
@@ -1,2 +1,1 @@
 The tool for create a base project from the template.
-https://github.com/cocos2d/cocos2d-x/tree/v2/cocos2dx/textures
--- Chengdu/texture/texture.go
+++ Chengdu/texture/texture.go
@@ -21,9 +21,10 @@ THE SOFTWARE.
 package texture
 
 import (
-	"github.com/golang/mobile/gl/glutil"
+	"fmt"
 	"github.com/lewgun/mobile/geom"
 	"github.com/lewgun/mobile/gl"
+	"github.com/lewgun/mobile/gl/glutil"
 )
 
 //Texture2DPixelFormat is stand for possible texture pixel formats
@@ -112,6 +113,9 @@ type Texture struct {
 
 func (t *Texture) String() string {
 
+	return fmt.Sprintf("<CCTexture2D | Name = %u | Dimensions = %u x %u | Coordinates = (%.2f, %.2f)>",
+		t.Name, t.PixelsWide, t.PixelsHigh, t.MaxS, t.MaxT)
+
 }
 
 func (t *Texture) releaseData(data []byte) {
@@ -138,6 +142,27 @@ Note that RGBA type textures will have their alpha premultiplied - use the blend
 */
 /** Initializes a texture from a glutil.Image object */
 func (t *Texture) initWithImage(img *glutil.Image) bool {
+	if img == nil {
+		//CCLOG("cocos2d: CCTexture2D. Can't create Texture. UIImage is nil");
+		return false;
+	}
+
+
+	//imageWidth := img->getWidth();
+	//imageHeight := img->getHeight();
+
+	CCConfiguration *conf = CCConfiguration::sharedConfiguration();
+
+ maxTextureSize := conf.getMaxTextureSize()
+if imageWidth > maxTextureSize || imageHeight > maxTextureSize {
+
+//CCLOG("cocos2d: WARNING: Image (%u x %u) is bigger than the supported %u x %u", imageWidth, imageHeight, maxTextureSize, maxTextureSize);
+return false
+}
+
+// always load premultiplied images
+return initPremultipliedATextureWithImage(uiImage, imageWidth, imageHeight);
+
 }
 
 //initWithData Initializes with a texture2d with data
@@ -148,10 +173,88 @@ func (t *Texture) initWithData(
 	pixelsHigh uint,
 	contentSize geom.Size) bool {
 
+	var bitsPerPixel uint
+	//Hack: bitsPerPixelForFormat returns wrong number for RGB_888 textures. See function.
+	if pixelFormat == Texture2DPixelFormat_RGB888 {
+		bitsPerPixel = 24
+	} else {
+		bitsPerPixel = bitsPerPixelForFormat(pixelFormat)
+	}
+
+	bytesPerRow := pixelsWide * bitsPerPixel / 8
+
+	switch {
+	case bytesPerRow%8 == 0:
+		//glPixelStorei(GL_UNPACK_ALIGNMENT, 8)
+
+	case bytesPerRow%4 == 0:
+		//glPixelStorei(GL_UNPACK_ALIGNMENT, 4)
+	case bytesPerRow%2 == 0:
+		//glPixelStorei(GL_UNPACK_ALIGNMENT, 2)
+	default:
+		//glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+
+	}
+
+	//glGenTextures(1, &m_uName)
+	//ccGLBindTexture2D(m_uName)
+	//
+	//glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR )
+	//glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR )
+	//glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE )
+	//glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE )
+
+	// Specify OpenGL texture image
+
+	switch pixelFormat {
+	case Texture2DPixelFormat_RGBA8888:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
+
+	case Texture2DPixelFormat_RGB888:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_RGB, GL_UNSIGNED_BYTE, data)
+
+	case Texture2DPixelFormat_RGBA4444:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_RGBA, GL_UNSIGNED_SHORT_4_4_4_4, data)
+
+	case Texture2DPixelFormat_RGB5A1:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_RGBA, GL_UNSIGNED_SHORT_5_5_5_1, data)
+
+	case Texture2DPixelFormat_RGB565:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_RGB, GL_UNSIGNED_SHORT_5_6_5, data)
+
+	case Texture2DPixelFormat_AI88:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE_ALPHA, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, data)
+
+	case Texture2DPixelFormat_A8:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_ALPHA, GL_UNSIGNED_BYTE, data)
+
+	case Texture2DPixelFormat_I8:
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, (GLsizei)pixelsWide, (GLsizei)pixelsHigh, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, data)
+
+	default:
+		//CCAssert(0, "NSInternalInconsistencyException")
+
+	}
+
+	t.ContentSize = contentSize
+	t.PixelsWide = pixelsWide
+	t.PixelsHigh = pixelsHigh
+	t.PixelFormat = pixelFormat
+	t.MaxS = contentSize.width / float32(pixelsWide)
+	t.MaxT = contentSize.height / float32(pixelsHigh)
+
+	t.HasPremultipliedAlpha = false
+	t.HasMipmaps = false
+
+	//setShaderProgram(CCShaderCache::sharedShaderCache()->programForKey(Shader_PositionTexture))
+
+	return true
+
 }
 
 /** Initializes a texture from a PVR file */
 func (t *Texture) initWithPVRFile(file string) bool {
+
 }
 
 /** Initializes a texture from a ETC file */
@@ -190,16 +293,192 @@ func (t *Texture) ContentSizeInPixels() geom.Size {
 
 }
 
-func (t *Texture) HasPremultipliedAlpha() {
-
-}
-
-func (t *Texture) HasMipmaps() {}
-
 func (t *Texture) initPremultipliedATextureWithImage(
 	image *glutil.Image,
 	pixelsWide uint,
 	pixelsHigh uint) bool {
+
+            tempData := image->getData();
+
+			var (
+inPixel32 *uint32
+inPixel8 *uint8
+outPixel16 *uint16
+pixelFormat CCTexture2DPixelFormat
+			)
+
+bool                      hasAlpha := image->hasAlpha();
+CCSize                    imageSize := CCSizeMake((float)(image->getWidth()), (float)(image->getHeight()));
+CCTexture2DPixelFormat    pixelFormat;
+size_t                    bpp = image->getBitsPerComponent();
+
+// compute pixel format
+if (hasAlpha)
+{
+pixelFormat = g_defaultAlphaPixelFormat;
+}
+else
+{
+if (bpp >= 8)
+{
+pixelFormat = kCCTexture2DPixelFormat_RGB888;
+}
+else
+{
+pixelFormat = kCCTexture2DPixelFormat_RGB565;
+}
+
+}
+
+// Repack the pixel data into the right format
+unsigned int length = width * height;
+
+if (pixelFormat == kCCTexture2DPixelFormat_RGB565)
+{
+if (hasAlpha)
+{
+// Convert "RRRRRRRRRGGGGGGGGBBBBBBBBAAAAAAAA" to "RRRRRGGGGGGBBBBB"
+
+tempData = new unsigned char[width * height * 2];
+outPixel16 = (unsigned short*)tempData;
+inPixel32 = (unsigned int*)image->getData();
+
+for(unsigned int i = 0; i < length; ++i, ++inPixel32)
+{
+*outPixel16++ =
+((((*inPixel32 >>  0) & 0xFF) >> 3) << 11) |  // R
+((((*inPixel32 >>  8) & 0xFF) >> 2) << 5)  |  // G
+((((*inPixel32 >> 16) & 0xFF) >> 3) << 0);    // B
+}
+}
+else
+{
+// Convert "RRRRRRRRRGGGGGGGGBBBBBBBB" to "RRRRRGGGGGGBBBBB"
+
+tempData = new unsigned char[width * height * 2];
+outPixel16 = (unsigned short*)tempData;
+inPixel8 = (unsigned char*)image->getData();
+
+for(unsigned int i = 0; i < length; ++i)
+{
+*outPixel16++ =
+(((*inPixel8++ & 0xFF) >> 3) << 11) |  // R
+(((*inPixel8++ & 0xFF) >> 2) << 5)  |  // G
+(((*inPixel8++ & 0xFF) >> 3) << 0);    // B
+}
+}
+}
+else if (pixelFormat == kCCTexture2DPixelFormat_RGBA4444)
+{
+// Convert "RRRRRRRRRGGGGGGGGBBBBBBBBAAAAAAAA" to "RRRRGGGGBBBBAAAA"
+
+inPixel32 = (unsigned int*)image->getData();
+tempData = new unsigned char[width * height * 2];
+outPixel16 = (unsigned short*)tempData;
+
+for(unsigned int i = 0; i < length; ++i, ++inPixel32)
+{
+*outPixel16++ =
+((((*inPixel32 >> 0) & 0xFF) >> 4) << 12) | // R
+((((*inPixel32 >> 8) & 0xFF) >> 4) <<  8) | // G
+((((*inPixel32 >> 16) & 0xFF) >> 4) << 4) | // B
+((((*inPixel32 >> 24) & 0xFF) >> 4) << 0);  // A
+}
+}
+else if (pixelFormat == kCCTexture2DPixelFormat_RGB5A1)
+{
+// Convert "RRRRRRRRRGGGGGGGGBBBBBBBBAAAAAAAA" to "RRRRRGGGGGBBBBBA"
+inPixel32 = (unsigned int*)image->getData();
+tempData = new unsigned char[width * height * 2];
+outPixel16 = (unsigned short*)tempData;
+
+for(unsigned int i = 0; i < length; ++i, ++inPixel32)
+{
+*outPixel16++ =
+((((*inPixel32 >> 0) & 0xFF) >> 3) << 11) | // R
+((((*inPixel32 >> 8) & 0xFF) >> 3) <<  6) | // G
+((((*inPixel32 >> 16) & 0xFF) >> 3) << 1) | // B
+((((*inPixel32 >> 24) & 0xFF) >> 7) << 0);  // A
+}
+}
+else if (pixelFormat == kCCTexture2DPixelFormat_A8)
+{
+// Convert "RRRRRRRRRGGGGGGGGBBBBBBBBAAAAAAAA" to "AAAAAAAA"
+inPixel32 = (unsigned int*)image->getData();
+tempData = new unsigned char[width * height];
+unsigned char *outPixel8 = tempData;
+
+for(unsigned int i = 0; i < length; ++i, ++inPixel32)
+{
+*outPixel8++ = (*inPixel32 >> 24) & 0xFF;  // A
+}
+}
+
+if (hasAlpha && pixelFormat == kCCTexture2DPixelFormat_RGB888)
+{
+// Convert "RRRRRRRRRGGGGGGGGBBBBBBBBAAAAAAAA" to "RRRRRRRRGGGGGGGGBBBBBBBB"
+inPixel32 = (unsigned int*)image->getData();
+tempData = new unsigned char[width * height * 3];
+unsigned char *outPixel8 = tempData;
+
+for(unsigned int i = 0; i < length; ++i, ++inPixel32)
+{
+*outPixel8++ = (*inPixel32 >> 0) & 0xFF; // R
+*outPixel8++ = (*inPixel32 >> 8) & 0xFF; // G
+*outPixel8++ = (*inPixel32 >> 16) & 0xFF; // B
+}
+}
+
+initWithData(tempData, pixelFormat, width, height, imageSize);
+
+if (tempData != image->getData())
+{
+delete [] tempData;
+}
+
+m_bHasPremultipliedAlpha = image->isPremultipliedAlpha();
+return true;
+
+}
+
+//StringForFormat returns the pixel format.
+func (t *Texture) StringForFormat() string {
+
+	switch t.PixelFormat {
+	case Texture2DPixelFormat_RGBA8888:
+		return "RGBA8888"
+
+	case Texture2DPixelFormat_RGB888:
+		return "RGB888"
+
+	case Texture2DPixelFormat_RGB565:
+		return "RGB565"
+
+	case Texture2DPixelFormat_RGBA4444:
+		return "RGBA4444"
+
+	case Texture2DPixelFormat_RGB5A1:
+		return "RGB5A1"
+
+	case Texture2DPixelFormat_AI88:
+		return "AI88"
+
+	case Texture2DPixelFormat_A8:
+		return "A8"
+
+	case Texture2DPixelFormat_I8:
+		return "I8"
+
+	case Texture2DPixelFormat_PVRTC4:
+		return "PVRTC4"
+
+	case Texture2DPixelFormat_PVRTC2:
+		return "PVRTC2"
+
+	default:
+		panic("unrecognized pixel format")
+
+	}
 
 }
 
@@ -217,13 +496,13 @@ How does it work ?
 This parameter is not valid for PVR / PVR.CCZ images.
 
 */
-//static void setDefaultAlphaPixelFormat(CCTexture2DPixelFormat format);
+//static void setDefaultAlphaPixelFormat(CCTexture2DPixelFormat format)
 
 /** returns the alpha pixel format
 @since v0.8
 @js getDefaultAlphaPixelFormat
 */
-//static CCTexture2DPixelFormat defaultAlphaPixelFormat();
+//static CCTexture2DPixelFormat defaultAlphaPixelFormat()
 
 /** treats (or not) PVR files as if they have alpha premultiplied.
 Since it is impossible to know at runtime if the PVR images have the alpha channel premultiplied, it is
@@ -233,15 +512,12 @@ By default it is disabled.
 
 @since v0.99.5
 */
-//static void PVRImagesHavePremultipliedAlpha(bool haveAlphaPremultiplied);
+//static void PVRImagesHavePremultipliedAlpha(bool haveAlphaPremultiplied)
 
-//// returns the pixel format.
-//const char* stringForFormat();
-
-////void* keepData(void *data, unsigned int length);
+////void* keepData(void *data, unsigned int length)
 ///** Initializes a texture from a string with dimensions, alignment, font name and font size */
-//bool initWithString(const char *text,  const char *fontName, float fontSize, const CCSize& dimensions, CCTextAlignment hAlignment, CCVerticalTextAlignment vAlignment);
+//bool initWithString(const char *text,  const char *fontName, float fontSize, const CCSize& dimensions, CCTextAlignment hAlignment, CCVerticalTextAlignment vAlignment)
 ///** Initializes a texture from a string with font name and font size */
-//bool initWithString(const char *text, const char *fontName, float fontSize);
+//bool initWithString(const char *text, const char *fontName, float fontSize)
 ///** Initializes a texture from a string using a text definition*/
-//bool initWithString(const char *text, ccFontDefinition *textDefinition);
+//bool initWithString(const char *text, ccFontDefinition *textDefinition)
